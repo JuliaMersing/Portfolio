2c9140062a0f3927a5a616196d59818ccbcb2466
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-07-08 15:50:08 +0200

    change link

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,64 +1,48 @@
 const projects = [
   {
     name: "La Colmena",
-    description: "Web de empresa basada en mobile first con responsive. #html",
-    url: "https://github.com/Adalab/modulo-1-evaluacion-final-JuliaMersing",
-    emoji: "🚀",
-    id: 1,
-  },
-
-  {
-    name: "La Colmena",
     description:
       "Web de empresa basada en mobile first con responsive. #html #saas",
-    url: "https://github.com/Adalab/project-promo-m-module-1-team-4",
+    url: "https://github.com/JuliaMersing/project-promo-m-module-1-team-4",
     emoji: "🐝",
-    id: 2,
+    id: 1,
   },
   {
     name: "AwesomeCards",
     description:
       "App de intercambio de tarjetas de contacto. #vanilla #javascript",
-    url: "https://github.com/Adalab/project-promo-m-module-2-team-3",
+    url: "https://github.com/JuliaMersing/project-promo-m-module-2-team-3",
     emoji: "👩🏼‍💻",
-    id: 3,
+    id: 2,
   },
   {
     name: "Listen to my Card",
     description: "Proyecto heredado y refactorizado a #react",
-    url: "https://github.com/Adalab/project-promo-m-module-3-team-2",
+    url: "https://github.com/JuliaMersing/project-promo-m-module-3-team-2",
     emoji: "🎵",
-    id: 4,
+    id: 3,
   },
   {
     name: "TVShows",
     description: "Gestor de series favoritas #javascript #localstorage #api",
-    url: "https://github.com/Adalab/modulo-2-evaluacion-final-JuliaMersing",
+    url: "https://github.com/JuliaMersing/modulo-2-evaluacion-final-JuliaMersing",
     emoji: "📺",
-    id: 5,
+    id: 4,
   },
   {
     name: "Ricky&Morty",
     description:
       "Buscador de personajes Ricky and Morty. #filters #React-rooter",
-    url: "https://github.com/Adalab/modulo-3-evaluacion-final-JuliaMersing",
+    url: "https://github.com/JuliaMersing/modulo-3-evaluacion-final-JuliaMersing",
     emoji: "👽",
-    id: 6,
+    id: 5,
   },
   {
-    name: "AdivinaElNúmero",
-    description:
-      "Juego para adivinar un número aleatorio #javascript #brutalist",
-    url: "https://github.com/Adalab/modulo-2-evaluacion-intermedia-JuliaMersing",
-    emoji: "🧮",
-    id: 7,
-  },
-  {
-    name: "Linkedin Users",
+    name: "Lista Pokemon",
     description: "Buscador de usuarios y repos de github #javascript",
-    url: "https://github.com/Adalab/modulo-3-evaluacion-intermedia-JuliaMersing",
+    url: "https://github.com/JuliaMersing/project-promo-m-module-2-team-3",
     emoji: "🧾",
-    id: 8,
+    id: 6,
   },
 ];
 
